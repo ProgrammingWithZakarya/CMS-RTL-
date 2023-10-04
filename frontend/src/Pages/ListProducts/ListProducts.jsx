a293e32791906b4cb8180c24c0657f533c730a62
Zakarya HasanZadeh <hasanzadahzakarya@gmail.com>
2023-10-04 12:58:04 +0330

    create ListProducts page (base)

---
+++ frontend/src/Pages/ListProducts/ListProducts.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function ListProducts (){
+    return (
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis dolore porro dolorum inventore consectetur debitis maiores molestias quam accusamus! Neque commodi veritatis earum ullam exercitationem. Deleniti magni repudiandae eaque necessitatibus.</p>
+    )
+}
