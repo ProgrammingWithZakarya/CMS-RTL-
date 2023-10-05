f2298a508e9c811bc300748b60c4ec04dc05180c
Zakarya HasanZadeh <hasanzadahzakarya@gmail.com>
2023-10-05 15:36:29 +0330

    refactoring based on parent component

--- frontend/src/Components/Modals/AddTodoModal/AddTodoModal.jsx
+++ frontend/src/Components/Modals/AddTodoModal/AddTodoModal.jsx
@@ -1,21 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContentData from '../../../content-data';
-import { users } from '../../../Datas';
-export default function AddTodoModal({ header, desc }) {
+export default function AddTodoModal({ header, desc, isShowModal, setAddTodoModal }) {
     const contextData = useContext(ContentData)
+    const [todoTitle, setTodoTitle] = useState('')
     const addTodoHandler = () => {
         let newTodo = {
-            id: localStorage.getItem.length + 1,
-            title: contextData.addTodoValue,
+            id: JSON.parse(localStorage.getItem('todos')).length + 1,
+            title: todoTitle,
             isCompleted: false
         }
-        users.admins[1].todos.push(newTodo)
-        contextData.setAddTodoModal(false)
-        contextData.setAddTodoValue('')
+        let localStorageSaved = JSON.parse(localStorage.getItem('todos'))
+        localStorage.setItem('todos', JSON.stringify([...localStorageSaved, newTodo]))
+        setAddTodoModal(false)
     }
     return (
         <>
-            {contextData.addTodoModal && (
+            {isShowModal && (
                 <div className='modal-cotainer flex justify-center items-center'>
                     <div className={`modal-delete w-[650px] h-[250px] rounded-lg flex flex-col justify-between ${contextData.componentStyle} p-3`}>
                         <div className="headerModal">
@@ -27,8 +27,8 @@ export default function AddTodoModal({ header, desc }) {
                             <div className="text-center">
                                 <input
                                     type="text"
-                                    value={contextData.addTodoValue}
-                                    onChange={(e) => contextData.setAddTodoValue(e.target.value)}
+                                    value={todoTitle}
+                                    onChange={(e) => setTodoTitle(e.target.value)}
                                     className='input-children'
                                 />
                             </div>
@@ -36,7 +36,7 @@ export default function AddTodoModal({ header, desc }) {
                         <div className="btns-modal flex items-center overflow-hidden h-16 justify-end gap-2">
                             <div className="w-1/2"></div>
                             <div className="h-full w-1/2  flex justify-end gap-2">
-                                <button className='w-auto h-full bg-red-700 text-white font-bold px-4 rounded hover:brightness-75' onClick={() => contextData.setAddTodoModal(false)}>لغو</button>
+                                <button className='w-auto h-full bg-red-700 text-white font-bold px-4 rounded hover:brightness-75' onClick={() => setAddTodoModal(false)}>لغو</button>
                                 <button className='w-auto h-full bg-palette-700 text-white font-bold px-4 grow rounded hover:brightness-75' onClick={() => addTodoHandler()}>تایید</button>
                             </div>
                         </div>
